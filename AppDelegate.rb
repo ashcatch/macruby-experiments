b5d4c03e895b1682d9f3850f11a2b5e6b35d2270
Rainer Schmid <rms@froglogic.com>
2011-02-06 17:57:36 +0100

    add a timer to time the tea

--- AppDelegate.rb
+++ AppDelegate.rb
@@ -1,6 +1,14 @@
 class AppDelegate
   def applicationDidFinishLaunching(notification)
-    puts "Snafu!"
     NSApp.hide nil
+    NSTimer.scheduledTimerWithTimeInterval 5,
+      target:self,
+      selector:'teaReady:',
+      userInfo:nil,
+      repeats:false
+  end
+
+  def teaReady(theTimer)
+    NSApp.activateIgnoringOtherApps true
   end
 end

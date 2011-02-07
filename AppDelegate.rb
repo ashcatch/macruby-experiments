7e8867dbe6fcb254eadc4ca3ff96fd07438bce0c
Rainer Schmid <rms@froglogic.com>
2011-02-07 10:59:22 +0100

    add status item

--- AppDelegate.rb
+++ AppDelegate.rb
@@ -1,15 +1,34 @@
+$teaTimeSeconds = 5
+
 class AppDelegate
   def applicationDidFinishLaunching(notification)
     NSApp.hide nil
-    NSTimer.scheduledTimerWithTimeInterval 5,
+    NSTimer.scheduledTimerWithTimeInterval $teaTimeSeconds,
       target:self,
       selector:'teaReady:',
       userInfo:nil,
       repeats:false
+
+    @statusItem = NSStatusBar.systemStatusBar.statusItemWithLength NSVariableStatusItemLength
+    @timeFinished = Time.now + $teaTimeSeconds
+    @countDownTimer = NSTimer.scheduledTimerWithTimeInterval 1,
+      target:self,
+      selector:'countDown:',
+      userInfo:nil,
+      repeats:true
+
+    self.countDown(nil)
+  end
+
+  def countDown(theTime)
+    @statusItem.setTitle "☕ #{(@timeFinished - Time.now).round.to_i}"
   end
 
   def teaReady(theTimer)
+    @countDownTimer.invalidate
+    @statusItem.setTitle "☕ fertig!"
     NSApp.activateIgnoringOtherApps true
+
     NSAlert.alertWithMessageText(
       'Tee ist fertig!',
       defaultButton:nil,
